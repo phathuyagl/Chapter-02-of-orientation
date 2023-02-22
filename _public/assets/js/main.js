30442ae47121a9320f16091d63784858b0517438
DESKTOP-HVB515S\PhatHuy <huynh_phat_huy@allgrow-labo.jp>
2023-02-22 17:38:35 +0700

    update

--- _public/assets/js/main.js
+++ _public/assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("main.js");
+const navItems = document.querySelectorAll('.c-header__item');
+
+navItems.forEach((ele) => {
+    ele.addEventListener('click',(e) => {
+        if (e.target.className == 'is-active') {
+            e.preventDefault();
+        }
+    })
+})
+
